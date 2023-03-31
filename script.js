28c3ac513ad47473a06afe599a1a3b9593fa109b
aloooy108 <ali.zaki.alomran@gmail.com>
2023-03-31 06:33:57 +0300

    wrote comments to lay the ground.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("123");
+/*a func that generate a random number
+between 1 to 3 for rock, paper, and scissors.
+*/
+
+/*a func that take user input. should be
+case insensitive 
+*/
+
+/*a func that compares 2 inputs and declares
+the winner.
+*/
